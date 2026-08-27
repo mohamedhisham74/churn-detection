df1fe1d44d01ebf16578d764991e3818516f4b9b
mohamedhisham74 <mohamed.hishamCS21@fcai.usc.edu.eg>
2026-08-27 14:43:53 +0300

    first commit

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import Load_dotenv 
+import joblip 
+
+Load_dotenv(override=True)
+
+APP_NAME = os.getenv("APP_NAME")
+VERSION = os.getenv("APP_VERSION")
+SECRET_KEY_TOKEN= os.getenv("API_SECRET_KEY")
+
+
+BASE_DIR=os.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ARTIFAVTS_FOLDER_PATH=os.path.join(BASE_DIR,"artifacts")
+
+joblip.load(os.path.join(ARTIFAVTS_FOLDER_PATH,'preporcessor.pkl'))
+joblip.load(os.path.join(ARTIFAVTS_FOLDER_PATH,'forest_tuned.pkl'))
+
